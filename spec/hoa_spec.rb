fb6a5dfd38121baee8d73443b1c142d5f7ba7206
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-10-11 22:49:46 +0000

    Done.

--- spec/hoa_spec.rb
+++ spec/hoa_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'The HoA can be updated by' do
   describe 'the add_character method' do
-    it 'adds Snarf to :third_earthers' do
+    it 'adds Dave to :chipmunks' do
       expect(add_character(:chipmunks, "Dave")).to eq(["Alvin", "Simon", "Theodore", "Dave"])
     end
 
@@ -10,7 +10,7 @@ describe 'The HoA can be updated by' do
       expect(add_character(:third_earthers, "Snarf")).to eq(["Lion-O", "Cheetara", "Mumm-Ra (the ever-living)", "Snarf"])
     end
 
-    it 'adds Snarf to :jetsons' do
+    it 'adds Astro to :jetsons' do
       expect(add_character(:jetsons, "Astro")).to eq(["George", "Jane", "Judy", "Elroy", "Astro"])
     end
   end
